3729d3e437ac523202b063249f2b2c1be6ddf7b8
DSoutter <duncan.soutter@gmail.com>
2021-08-10 16:30:13 +0100

    added a couple more animal methods

--- console.py
+++ console.py
@@ -8,8 +8,10 @@ import repositories.animal_repo as animals_repo
 
 employee1= Employee("Frank", "11/08/2021", "Bird Keeper", 5)
 employee2= Employee("Morty","09/08/2021", "Apiary", 1, 10)
-employee_repo.add_employee(employee1)
+# employee_repo.add_employee(employee1)
 
+animal1 = Animal("Sergei", "Bear")
+animals_repo.add_animal(animal1)
 # employee_repo.list_employees()
 
 # employee_repo.find_employee(3)
